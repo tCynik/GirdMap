fbdaaf3003541013810f17ab2f3aa0b5438017a3
unknown <assir@inbox.ru>
2022-12-30 06:37:33 +0700

    finaly transition and cell-separation math & logics are ready! Important: need to test one by jUnit

--- src/main/java/com/example/lessongirdrecycler/domain/models/segment/CellTransition.kt
+++ src/main/java/com/example/lessongirdrecycler/domain/models/segment/CellTransition.kt
@@ -2,153 +2,158 @@ package com.example.lessongirdrecycler.domain.models.segment
 
 import com.example.lessongirdrecycler.domain.models.cell.CellLocation
 
-sealed class CellTransition (val cellSize: Int, val startCellLocation: CellLocation, endSegmentLocation: CellLocation){
-    abstract fun getAngleFromStartToCorner(): Int
-    class North(cellSize: Int, startCellLocation: CellLocation, endSegmentLocation: CellLocation)
-        : CellTransition(cellSize, startCellLocation, endSegmentLocation) {
-        override fun getAngleFromStartToCorner(): Int {
+// TODO: when implement into project need to test maths & logics by jUnit!!!
+sealed class CellTransition (val cellSize: Int){
+    abstract fun getAngleFromStartToCorner(startCellLocation: CellLocation): Int
+
+    private fun calculateByX(startCellLocation: CellLocation, endSegmentLocation: CellLocation): Int {
+        return ((startCellLocation.y/endSegmentLocation.y)*(endSegmentLocation.x)) + startCellLocation.x
+    }
+
+    private fun calculateByY(startCellLocation: CellLocation, endSegmentLocation: CellLocation): Int {
+        return ((startCellLocation.x/endSegmentLocation.x)*(endSegmentLocation.y)) + startCellLocation.y
+    }
+
+    class North(cellSize: Int)
+        : CellTransition(cellSize) {
+        override fun getAngleFromStartToCorner(startCellLocation: CellLocation): Int {
             return 0
         }
     }
 
-    class NE(cellSize: Int, startCellLocation: CellLocation, endSegmentLocation: CellLocation)
-        : CellTransition(cellSize, startCellLocation, endSegmentLocation) {
-        override fun getAngleFromStartToCorner(): Int {
+    class NE(cellSize: Int)
+        : CellTransition(cellSize) {
+        override fun getAngleFromStartToCorner(startCellLocation: CellLocation): Int {
             return (cellSize - startCellLocation.x)/startCellLocation.y
         }
     }
 
-    class East(cellSize: Int,
-                startCellLocation: CellLocation,
-                endSegmentLocation: CellLocation)
-        : CellTransition(cellSize, startCellLocation, endSegmentLocation) {
-        override fun getAngleFromStartToCorner(): Int {
+    class East(cellSize: Int)
+        : CellTransition(cellSize) {
+        override fun getAngleFromStartToCorner(startCellLocation: CellLocation): Int {
             return 0
         }
     }
 
-    class SE(cellSize: Int,
-                startCellLocation: CellLocation,
-                endSegmentLocation: CellLocation)
-        : CellTransition(cellSize, startCellLocation, endSegmentLocation) {
-        override fun getAngleFromStartToCorner(): Int {
+    class SE(cellSize: Int)
+        : CellTransition(cellSize) {
+        override fun getAngleFromStartToCorner(startCellLocation: CellLocation): Int {
             return (cellSize - startCellLocation.x)/(cellSize - startCellLocation.y)
         }
     }
 
-    class South(cellSize: Int,
-                startCellLocation: CellLocation,
-                endSegmentLocation: CellLocation)
-        : CellTransition(cellSize, startCellLocation, endSegmentLocation) {
-        override fun getAngleFromStartToCorner(): Int {
+    class South(cellSize: Int)
+        : CellTransition(cellSize) {
+        override fun getAngleFromStartToCorner(startCellLocation: CellLocation): Int {
             return 0
         }
     }
 
-    class SW(cellSize: Int,
-                startCellLocation: CellLocation,
-                endSegmentLocation: CellLocation)
-        : CellTransition(cellSize, startCellLocation, endSegmentLocation) {
-        override fun getAngleFromStartToCorner(): Int {
+    class SW(cellSize: Int)
+        : CellTransition(cellSize) {
+        override fun getAngleFromStartToCorner(startCellLocation: CellLocation): Int {
             return startCellLocation.x/(cellSize - startCellLocation.y)
         }
     }
 
-    class West(cellSize: Int,
-                startCellLocation: CellLocation,
-                endSegmentLocation: CellLocation)
-        : CellTransition(cellSize, startCellLocation, endSegmentLocation) {
-        override fun getAngleFromStartToCorner(): Int {
+    class West(cellSize: Int)
+        : CellTransition(cellSize) {
+        override fun getAngleFromStartToCorner(startCellLocation: CellLocation): Int {
             return 0
         }
     }
 
-    class NW(cellSize: Int,
-                startCellLocation: CellLocation,
-                endSegmentLocation: CellLocation)
-        : CellTransition(cellSize, startCellLocation, endSegmentLocation) {
-        override fun getAngleFromStartToCorner(): Int {
+    class NW(cellSize: Int)
+        : CellTransition(cellSize) {
+        override fun getAngleFromStartToCorner(startCellLocation: CellLocation): Int {
             return startCellLocation.x/startCellLocation.y
         }
     }
 
-    fun getTransitionLocations(cellTransition: CellTransition, startSegmentCellLocation, endSegmentCellLocation): List<CellLocation> {
+    fun getTransitionLocations(cellTransition: CellTransition,
+                               startCellLocation: CellLocation,
+                               endCellLocation: CellLocation): List<CellLocation> {
+
         val endSegmentLocation = CellLocation(
-            x = endSegmentCellLocation.x - startCellLocation.x,
-            y = endSegmentCellLocation.y - startCellLocation.y)
-        var endX: Int = 0
-        var endY: Int = 0
-        var nextStartX: Int = 0
-        var nextStartY: Int = 0
+            x = endCellLocation.x - startCellLocation.x,
+            y = endCellLocation.y - startCellLocation.y)
         val rateToSegmentEnd = endSegmentLocation.x/endSegmentLocation.y
+        val transitionLocations = mutableListOf<CellLocation>()
         when (cellTransition) {
             is North -> {
-                endX = endSegmentCellLocation.x
-                endY = 0
-                nextStartX = endX
-                nextStartY = cellSize
+                transitionLocations[0] = CellLocation(x = endCellLocation.x, y = 0)
+                transitionLocations[1] = CellLocation(x = endCellLocation.x, y = cellSize)
             }
             is NE -> {
-                val rateToCellCorner = getAngleFromStartToCorner()
-                if (rateToSegmentEnd < rateToCellCorner) { // через верхнюю сторону
-                    // находим неполную сторону (тут это Х) через пропорцию
-                    // пропорция = "сколько всего"/"сколько осталось до границы"
-                    // сколько надо пройти = остаток * пропорция
-                    // сколько осталось от неполной стороны = сколько уже есть + сколько надо пройти
-                    endX = endSegmentLocation.y/startCellLocation.y*(endSegmentLocation.x) + startCellLocation.x
-                    endY = 0
-                    nextStartX = endX
-                    nextStartY = cellSize
+                val rateToCellCorner = getAngleFromStartToCorner(startCellLocation)
+                if (rateToSegmentEnd < rateToCellCorner) { // через верхнюю сторону -> считаем от Y
+                    // находим неполную сторону (тут это Х) через пропорцию подобных треугольников
+                    // сколько осталось от неполной стороны = сколько уже есть + сколько надо пройти: Х = Хнач + Хост
+                    // сколько надо пройти = остаток * пропорция: Xост = К * Xвсг
+                    // пропорция = "сколько всего"/"сколько осталось до границы": К = Yост / Yвсг
+                    // итого: Х = Хнач + Хвсг * (Yост / Yвсг)
+                    transitionLocations[0] = CellLocation(x = calculateByX(startCellLocation, endSegmentLocation), y = 0)
+                    transitionLocations[1] = CellLocation(x = calculateByX(startCellLocation, endSegmentLocation), y = cellSize)
                 } else if (rateToSegmentEnd > rateToCellCorner) { // через правую сторону
-                    endX = cellSize
-                    endY = endSegmentLocation.x/startCellLocation.x*(endSegmentLocation.y) + startCellLocation.y
+                    transitionLocations[0] = CellLocation(x = cellSize, y = calculateByY(startCellLocation, endSegmentLocation))
+                    transitionLocations[1] = CellLocation(x = 0, y = calculateByY(startCellLocation, endSegmentLocation))
                 } else { // через угол
-                    endX = cellSize
-                    endY = 0
-                    nextStartX = 0
-                    nextStartY = 0
+                    transitionLocations[0] = CellLocation(x = cellSize, y = 0)
+                    transitionLocations[1] = CellLocation(x = 0, y = 0)
                 }
             }
             is East -> {
-                endX = cellSize
-                endY = endSegmentCellLocation.y
+                transitionLocations[0] = CellLocation(x = cellSize, y = endCellLocation.y)
+                transitionLocations[1] = CellLocation(x = 0, y = endCellLocation.y)
             }
             is SE -> {
-                val rateToCellCorner = getAngleFromStartToCorner()
-                if (rateToSegmentEnd < rateToCellCorner) { // calculating y
-
-                } else if (rateToSegmentEnd > rateToCellCorner) {
-
+                val rateToCellCorner = getAngleFromStartToCorner(startCellLocation)
+                if (rateToSegmentEnd < rateToCellCorner) { // через бок -> считаем Y
+                    transitionLocations[0] = CellLocation(x = cellSize, y = calculateByY(startCellLocation, endSegmentLocation))
+                    transitionLocations[1] = CellLocation(x = 0, y = calculateByY(startCellLocation, endSegmentLocation))
+                } else if (rateToSegmentEnd > rateToCellCorner) { // через низ, считаем Х
+                    transitionLocations[0] = CellLocation(x = calculateByX(startCellLocation, endSegmentLocation), y = cellSize)
+                    transitionLocations[1] = CellLocation(x = calculateByX(startCellLocation, endSegmentLocation), y = 0)
+                } else { // углы равны, трек идет через грань
+                    transitionLocations[0] = CellLocation(x = cellSize, y = cellSize)
+                    transitionLocations[1] = CellLocation(x = 0, y = 0)
                 }
             }
             is South -> {
-                endX = endSegmentCellLocation.x
-                endY = cellSize
+                transitionLocations[0] = CellLocation(x = endCellLocation.x, y = cellSize)
+                transitionLocations[1] = CellLocation(x = endCellLocation.x, y = 0)
             }
             is SW -> {
-                val rateToCellCorner = getAngleFromStartToCorner()
+                val rateToCellCorner = getAngleFromStartToCorner(startCellLocation)
                 if (rateToSegmentEnd < rateToCellCorner) { // calculating y
-
-                } else if (rateToSegmentEnd > rateToCellCorner) {
-
+                    transitionLocations[0] = CellLocation(x = 0, y = calculateByY(startCellLocation, endSegmentLocation))
+                    transitionLocations[1] = CellLocation(x = cellSize, y = calculateByY(startCellLocation, endSegmentLocation))
+                } else if (rateToSegmentEnd > rateToCellCorner) { // calculating X
+                    transitionLocations[0] = CellLocation(x = calculateByX(startCellLocation, endSegmentLocation), y = cellSize)
+                    transitionLocations[1] = CellLocation(x = calculateByX(startCellLocation, endSegmentLocation), y = 0)
+                } else {
+                    transitionLocations[0] = CellLocation(x = 0, y = cellSize)
+                    transitionLocations[1] = CellLocation(x = cellSize, y = 0)
                 }
             }
             is West -> {
-                endX = 0
-                endY = endSegmentCellLocation.y
+                transitionLocations[0] = CellLocation(x = 0, y = endCellLocation.y)
+                transitionLocations[1] = CellLocation(x = cellSize, y = endCellLocation.y)
             }
             is NW -> {
-                val rateToCellCorner = getAngleFromStartToCorner()
-                if (rateToSegmentEnd < rateToCellCorner) { // calculating y
-
-                }else if (rateToSegmentEnd > rateToCellCorner) {
-                    
+                val rateToCellCorner = getAngleFromStartToCorner(startCellLocation)
+                if (rateToSegmentEnd < rateToCellCorner) { // calculating X
+                    transitionLocations[0] = CellLocation(x = calculateByX(startCellLocation, endSegmentLocation), y = 0)
+                    transitionLocations[1] = CellLocation(x = calculateByX(startCellLocation, endSegmentLocation), y = cellSize)
+                }else if (rateToSegmentEnd > rateToCellCorner) { // calculating by X
+                    transitionLocations[0] = CellLocation(x = calculateByX(startCellLocation, endSegmentLocation), y = 0)
+                    transitionLocations[1] = CellLocation(x = calculateByX(startCellLocation, endSegmentLocation), y = cellSize)
+                } else { // transition by corner
+                    transitionLocations[0] = CellLocation(x = 0, y = 0)
+                    transitionLocations[1] = CellLocation(x = cellSize, y = cellSize)
                 }
             }
         }
-        var transitionLocations = mutableListOf<CellLocation>()
-        transitionLocations[0] = CellLocation(x = endX, y = endY)
-        transitionLocations[1] = CellLocation(x = nextStartX, y = nextStartY)
         return transitionLocations
     }
 }
